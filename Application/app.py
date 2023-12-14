116b9142dd4bdf07c71162cda8fd7c06fc066909
bluejeans98 <jsk0821@kaist.ac.kr>
2023-12-14 22:28:22 +0900

    made directory structure

---
+++ Application/app.py
@@ -0,0 +1,161 @@
+import streamlit as st
+import time
+import pandas as pd
+import numpy as np
+import psycopg2
+from utils import *
+import pickle
+from rank_bm25 import BM25Okapi
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import openai
+import os
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+st.title('Find Your FTA Contents')
+st.write('Recommendation & Chatbot service for FTA related contents')
+
+
+signin, signup, service = st.tabs(['Sign In', 'Sign Up', 'service'])
+
+with signin:
+    # Sign In tab
+    st.subheader('Sign In')
+    
+    # 로그인 상태를 추적하는 플래그가 설정되어 있지 않으면 로그인 폼을 표시
+    if not st.session_state.get('logged_in'):
+        userid = st.text_input('ID:')
+        password = st.text_input('Pswd', type='password')
+
+        if st.button('Sign In'):
+            create_userstable()
+            hashed_pswd = make_hashes(password)
+
+            result = login_user(userid, check_hashes(password, hashed_pswd))
+            if result:
+                st.success('Logged In as {}'.format(userid))
+                st.session_state['logged_in'] = True
+                st.subheader('These are some contents you would like...')
+                st.write()
+        
+                with st.spinner('Please wait...'):
+                    # result -> [(userid, password, job, company, department, jobcontent, interest)]
+                    per_info = list(result[0])[2:]   # per_info = ex) ['public officer', 'MOTIE', 'east asia divison', 'economic cooperate with ASEAN and India', 'AI regulation']
+                    per_info_txt = ', '.join(per_info) # per_info 컴마로 구분된 하나의 텍스트로 합치기
+                    st.session_state['per_info_str'] = per_info_txt
+                    print(st.session_state['per_info_str'])
+                    personal_recommendation = similar_news_search(per_info_txt) # user에 따른 뉴스 추천
+                    print(personal_recommendation)
+                    st.write(personal_recommendation.iloc[0][0])  # 추천기사 출력
+                    time.sleep(5)
+                st.success('Done!')
+
+            else:
+                st.warning('Incorrect User ID or Password')
+                
+    if st.session_state.get('logged_in'):
+        st.write("You are logged in.")
+
+with signup:
+    # Sign Up tab
+    st.subheader('Create New Account')
+
+    userid = st.text_input('User ID:')
+    password = st.text_input('Password:', type='password')
+    job = st.text_input('Job:')
+    company = st.text_input('Company:')
+    department = st.text_input('Department:')
+    jobcontent = st.text_input('Job Content:')
+    interest = st.text_input('Current Interest:')
+
+    if st.button('Sign Up'):
+        create_userstable()
+        add_userdata(userid, make_hashes(password), job, company, department, jobcontent, interest)
+
+with service:
+    # service tab
+    st.sidebar.title('Service Category')
+    select_service = st.sidebar.selectbox(
+        'Choose service to use.', 
+        ['---SELECT---', 'Contents Recommendation', 'Chat with Chatbot'])
+
+    if str(select_service) == 'Contents Recommendation':
+        st.header(select_service)
+        st.write()
+        text = st.text_area(label='Input the keywords or contents that are related to what you want to find.',
+                            placeholder='ex) international dispute division')
+        num = st.text_area(label='Input the number of contents you want to find.',
+                        placeholder='ex) 3')
+        
+        if st.button('Search'):
+            if 'per_info_str' in st.session_state:
+                with st.spinner('Please wait...'):
+                    print(st.session_state['per_info_str'])
+                    recommendation = similar_news_search(st.session_state['per_info_str'], text, num)
+                    st.dataframe(recommendation)
+                    time.sleep(5)
+            
+            st.success('Done!')
+
+    elif str(select_service) == 'Chat with Chatbot':
+        st.header(select_service)
+        st.subheader('How can I help you today?')
+        st.write()
+        text = st.text_area(label='Ask to Chatbot...',
+                            placeholder='ex) Recommend some FTA contents related to international dispute division.')
+        if 'per_info_str' in st.session_state:
+            News = similar_news_search(st.session_state['per_info_str'], text)
+            Agreement = similar_agreement_search(st.session_state['per_info_str'], text)
+        question_full = f'''
+
+You will receive the agreement and news data that are most relevant to the question as context. Before answering, quote the relevant sentence in the agreement and news, as in word-by-word, before answering.
+
+[Context]:
+
+<<User>>
+
+{st.session_state['per_info_str']}
+
+<<Agreement>>
+
+{Agreement}
+
+<<News>>
+
+{News}
+
+[Question]:
+
+{text}
+
+[Answer]:
+
+'''
+
+        #question_full = '<User>' + st.session_state['per_info_str'] + '<Query>' + text + '<Agreement>' + Agreement + '<News>' + News
+        #question_full = question_full['Column'].str.cat(sep='\n')
+        print(question_full)
+        if st.button('Ask'):
+            with st.spinner('Please wait...'):
+                answer = chatbot_answer(question_full)
+                st.write(answer)
+                time.sleep(5)
+            st.success('Done!')
+
+    else:
+        st.header('Our ongoing service: ')
+
+        st.subheader('Contents Recommendation Service')
+        st.write('You can now easily get the FTA contents that you want ro find!')
+        st.subheader('Chat with Chatbot Service')
+        st.write('Start chatting with Chatbot and get what you want!')
+        st.subheader('Recommendation based on User Info')
+        st.write('You can get some contents related to you personal info')
+
+
+
+
+
---
+++ Application/utils.py
@@ -0,0 +1,174 @@
+import streamlit as st
+import pandas as pd
+import psycopg2
+import hashlib
+import chromadb
+import openai
+from openai import OpenAI
+import pickle
+from rank_bm25 import BM25Okapi
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import json
+import os
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+# Security
+#passlib,hashlib,bcrypt,scrypt
+def make_hashes(password):
+	return hashlib.sha256(str.encode(password)).hexdigest()
+
+def check_hashes(password,hashed_text):
+	if make_hashes(password) == hashed_text:
+		return hashed_text
+	return False
+
+# DB connection
+connection_info = "host=147.47.200.145 dbname=teamdb6 user=team6 password=qwer6 port=34543"
+# connection_info = "host=localhost user=postgres dbname=postgres password=744744ks! port=5432"
+
+def init_connection():
+    return psycopg2.connect(connection_info)
+
+def run_query(query):
+    try:
+        conn = init_connection()
+        df = pd.read_sql(query, conn)
+    except psycopg2.Error as e:
+        print('DB error: ', e)
+        conn.close()
+    finally:
+        conn.close()
+    return df
+
+def run_tx(query):
+    try:
+        conn = init_connection()
+        with conn.cursor() as cur:
+            cur.execute(query)
+    except psycopg2.Error as e:
+        print('DB error: ', e)
+        conn.rollback()
+        conn.close()
+    finally:
+        conn.commit()
+        conn.close()
+    return
+
+def create_userstable():
+    try:
+        conn = init_connection()
+        with conn.cursor() as cur:
+            cur.execute('CREATE TABLE IF NOT EXISTS userstable (userid TEXT, password TEXT,\
+                        job TEXT, company TEXT, department TEXT, jobcontent TEXT, interest TEXT,\
+                        PRIMARY KEY (userid))')
+            
+    except psycopg2.Error as e:
+        print('DB error: ', e)
+        conn.close()
+    finally:
+        conn.commit()
+        conn.close()
+
+# Insert information of newly signed up user to userstable(PostgreSQL DB)
+def add_userdata(userid, password, job, company, department, jobcontent, interest):
+    try:
+        conn = init_connection()
+        with conn.cursor() as cur:
+            cur.execute(f"SELECT * FROM userstable WHERE userid= '{userid}'")
+            if cur.fetchall():  # 이미 userid가 존재한다면
+                st.warning('User ID already exits')
+            else:
+                cur.execute(f"INSERT INTO userstable(userid, password, job, company, department, jobcontent, interest)\
+                            VALUES ('{userid}', '{password}', '{job}', '{company}', '{department}', '{jobcontent}', '{interest}')")
+                conn.commit()
+                st.success("You have successfully created a valid Account")
+                st.info('Go to Sign In menu to sign in')
+    except psycopg2.Error as e:
+        # 데이터베이스 에러 처리
+        print("DB error: ", e)
+        conn.rollback()
+        conn.close()
+    finally:
+        conn.commit()
+        conn.close()
+    return
+
+# function for Sign In
+def login_user(userid, password):
+    try:
+        conn = init_connection()
+        with conn.cursor() as cur:
+            cur.execute(f"SELECT * FROM userstable WHERE userid= '{userid}' AND password= '{password}'")
+            data = cur.fetchall()   # row: (userid, password, job, company, department, jobcontent, interest) 형태
+            
+    except psycopg2.Error as e:
+        print('DB error: ', e)
+        conn.close()
+    finally:
+        conn.close()
+    return data
+
+
+# similar news search
+def similar_news_search(user, query=None, num=1) :
+    chroma_client = chromadb.PersistentClient(path=r"C:\Users\admin\Downloads\FTA_code\chroma")
+    collection = chroma_client.get_collection(name='FTA_collection')
+    if query is not None:
+        input_n = user + query
+    else :
+        input_n = user
+    query_result = collection.query(query_texts=input_n, n_results=int(num))
+    documents = query_result['documents'][0]
+    News = pd.DataFrame(documents, columns=['Column'])
+    return News
+
+# bm25 preprocess
+def preprocess_text(text):
+    text = text.lower()
+    word_tokens = word_tokenize(text)
+    filtered_words = [word for word in word_tokens if word not in stopwords.words('english')]
+    return filtered_words
+
+# similar agreement search
+def similar_agreement_search(user, query=None) :
+    
+    agreements = []
+    with open(r'C:\Users\admin\Downloads\FTA_code\joined_chunks.pkl', 'rb') as file:
+        agreements_5000 = pickle.load(file)
+
+    with open(r"C:\Users\admin\Downloads\FTA_code\agreements_bm25_5000.pkl", 'rb') as file :
+        bm25 = pickle.load(file)
+        if query is not None:
+            input_a = user + query
+        else :
+            input_a = user
+            
+        preprocessed_query = preprocess_text(input_a)
+        doc_scores = bm25.get_scores(preprocessed_query)
+        max_score_index = doc_scores.argmax()
+        Agreements = agreements_5000[max_score_index]
+    return Agreements
+
+
+# function for chatbot service
+client = OpenAI()
+def chatbot_answer(question, model="gpt-4-1106-preview"):
+    
+
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": question}
+        ],
+        max_tokens=1500
+    )
+
+    return response.choices[0].message.content
+# 
+
+    
